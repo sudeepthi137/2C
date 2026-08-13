fd8a7ae33d02875c3f7957a0b32b48a4f198ec75
sudeepthi137 <kavilisudeepthi@gmail.com>
2026-08-13 19:15:24 +0530

    Add ButtonClickHandler component with click event

---
+++ index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function ButtonClickHandler() {
+    const handleClick = () => {
+        alert("Button was clicked!");
+    };
+
+    return (
+        <div>
+            <h2>Button Click Event</h2>
+            <button onClick={handleClick}>Button</button>
+        </div>
+    );
+}
+
+export default ButtonClickHandler;
